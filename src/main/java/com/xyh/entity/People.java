2c0f54bd9cbb45f33ef656895a1764b3872133bb
Irisx33 <46704084+irisx33@users.noreply.github.com>
2022-01-19 23:12:39 +0800

    mybaris初始化测试环境搭建

---
+++ src/main/java/com/xyh/entity/People.java
@@ -0,0 +1,15 @@
+package com.xyh.entity;
+
+import lombok.Data;
+
+/**
+ * @author xyh
+ * @version 1.0
+ */
+@Data
+public class People {
+    private Integer id;
+    private String name;
+    private Integer deptId;
+    private String password;
+}
